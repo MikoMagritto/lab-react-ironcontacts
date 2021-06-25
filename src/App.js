a3f3bd618c7e4e8b08863c5ec85f8be85dac7591
MikoMagritto <77416124+MikoMagritto@users.noreply.github.com>
2021-06-26 00:33:45 +0200

    iteration1

--- src/App.js
+++ src/App.js
@@ -1,59 +1,72 @@
 import React from 'react';
 import './App.css';
 import contacts from './contacts.json';
+import CardActor from './CardActor'
 
 
 class App extends React.Component {
-  state ={ 
-  actors:[
-  {"name": "Idris Elba",
-  "pictureUrl": "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-  "popularity": 11.622713,
-  "id": "11731993-0604-4bee-80d5-67ad845d0a38"
-},
-{
-  "name": "Johnny Depp",
-  "pictureUrl": "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
-  "popularity": 15.656534,
-  "id": "7dad00f7-3949-477d-a7e2-1467fd2cfc06"
-},
-{
-  "name": "Monica Bellucci",
-  "pictureUrl": "https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg",
-  "popularity": 16.096436,
-  "id": "0ad5e441-3084-47a1-9e9b-b917539bba71"
-},
-{
-  "name": "Gal Gadot",
-  "pictureUrl": "https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg",
-  "popularity": 10.049256,
-  "id": "b497e3c4-50bb-4ae2-912f-eb36802c5bc2"
-},
-{
-  "name": "Ian McKellen",
-  "pictureUrl": "https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg",
-  "popularity": 10.070132,
-  "id": "0067ae32-97b6-4431-898e-eb1c10150abb"
+  state={
+    actors:[contacts[0],contacts[1],contacts[2],contacts[3],contacts[4]]
+  }
 
-}]};
+  AddContact = () => {
+const randomIndexActors =Math.round(Math.random() * (contacts.length - 1) + 1);
+console.log("Hello",randomIndexActors);
+this.setState({
+actors: contacts[randomIndexActors]
+})
+};
     
   render() {
     console.log(this.state.actors);
     return (
       <div className="App" >
-     <table>
-    <thead>
-        <tr>
-            <th>Ironcontacts</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Picture{this.state.actors.name}</td>
-            <td>Name</td>
-            <td>Popularity</td>
-        </tr>
-    </tbody>
+      <button onClick={this.AddContact}>Add a random contact</button>
+   <table>
+  <thead>
+      <tr>
+          <th>Ironcontacts</th>
+      </tr>
+  </thead>
+  <tbody>
+      <tr>
+          <td>Picture</td>
+          <td>Name</td>
+          <td>Popularity</td>
+      </tr>
+      <tr>
+          <td><img src={contacts[0].pictureUrl}/></td>
+          <td>{contacts[0].name}</td>
+          <td>{contacts[0].popularity}</td>
+      </tr>
+      <tr>
+          <td><img src={contacts[1].pictureUrl}/></td>
+          <td>{contacts[1].name}</td>
+          <td>{contacts[1].popularity}</td>
+      </tr>
+      <tr>
+          <td><img src={contacts[2].pictureUrl}/></td>
+          <td>{contacts[2].name}</td>
+          <td>{contacts[2].popularity}</td>
+      </tr>
+      <tr>
+          <td><img src={contacts[3].pictureUrl}/></td>
+          <td>{contacts[3].name}</td>
+          <td>{contacts[3].popularity}</td>
+      </tr>
+      <tr>
+          <td><img src={contacts[4].pictureUrl}/></td>
+          <td>{contacts[4].name}</td>
+          <td>{contacts[4].popularity}</td>
+      </tr>
+      <tr>
+      {this.state.actors.map(actors =>{
+          return < CardActor key={actors.id}/>
+                                      }               
+                            )
+      } 
+      </tr>
+  </tbody>
 </table>
 </div>
     );
