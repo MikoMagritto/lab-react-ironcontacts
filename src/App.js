3e8c64d050d26a03cbb4f51fd6fb63b5690a31e4
Michel Hardy <michel.robin.hardy@gmail.com>
2021-06-26 10:04:22 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
 class App extends React.Component {
+
+
+
   render() {
     return (
       <div className="App" >
+        <h1>{this.contacts}</h1>
       </div>
     );
   }
