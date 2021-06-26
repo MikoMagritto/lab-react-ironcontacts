2bf544b5b401766b85f99e40cfad34a822cc9f85
Michel Hardy <michel.robin.hardy@gmail.com>
2021-06-26 11:12:34 +0200

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ class App extends React.Component {
 
   AddContact = () => {
     const randomIndexActors = Math.round(Math.random() * (contacts.length - 1) + 1);
+    const actorsCopy = this.state.actors;
+    actorsCopy.push(contacts[randomIndexActors])
     console.log("Hello", randomIndexActors);
     this.setState({
-      actors: contacts[randomIndexActors]
+      actors: actorsCopy
     })
   };
 
@@ -25,45 +27,19 @@ class App extends React.Component {
         <table>
           <thead>
             <tr>
-              <th>Ironcontacts</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
               <td>Picture</td>
               <td>Name</td>
               <td>Popularity</td>
             </tr>
-            <tr>
-              <td><img src={contacts[0].pictureUrl} /></td>
-              <td>{contacts[0].name}</td>
-              <td>{contacts[0].popularity}</td>
-            </tr>
-            <tr>
-              <td><img src={contacts[1].pictureUrl} /></td>
-              <td>{contacts[1].name}</td>
-              <td>{contacts[1].popularity}</td>
-            </tr>
-            <tr>
-              <td><img src={contacts[2].pictureUrl} /></td>
-              <td>{contacts[2].name}</td>
-              <td>{contacts[2].popularity}</td>
-            </tr>
-            <tr>
-              <td><img src={contacts[3].pictureUrl} /></td>
-              <td>{contacts[3].name}</td>
-              <td>{contacts[3].popularity}</td>
-            </tr>
-            <tr>
-              <td><img src={contacts[4].pictureUrl} /></td>
-              <td>{contacts[4].name}</td>
-              <td>{contacts[4].popularity}</td>
-            </tr>
-            <tr>
-              <td><img src={this.state.actors.pictureUrl} /></td>
-              <td>{this.state.actors.name}</td>
-              <td>{this.state.actors.popularity}</td>
-            </tr>
+          </thead>
+          <tbody>
+            {this.state.actors.map((actor, i) => {
+              return (<tr>
+                <td><img src={contacts[i].pictureUrl} /></td>
+                <td>{contacts[i].name}</td>
+                <td>{contacts[i].popularity}</td>
+              </tr>)
+            })}
           </tbody>
         </table>
       </div>
