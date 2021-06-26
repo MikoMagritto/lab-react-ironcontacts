92eecaaef0ac1ead2142e96e13e8ec6e3d6a2d6d
Michel Hardy <michel.robin.hardy@gmail.com>
2021-06-26 11:47:29 +0200

    terminado

--- src/App.js
+++ src/App.js
@@ -9,21 +9,65 @@ class App extends React.Component {
     actors: [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]
   }
 
+  delete = id => {
+    const actorsCopy = [...this.state.actors];
+    const actorIndex = actorsCopy.findIndex(item => item.id === id);
+    actorsCopy.splice(actorIndex, 1);
+
+    this.setState({
+      actors: actorsCopy,
+
+    })
+  }
+
+
+
+
+  SortPopularity = () => {
+    const actorsCopy = [...this.state.actors];
+    console.log("acteur", actorsCopy)
+    actorsCopy.sort(function (a, b) {
+      return b.popularity - a.popularity;
+    })
+    this.setState({
+      actors: actorsCopy
+    })
+  };
+
+  SortName = () => {
+    const actorsCopy = [...this.state.actors];
+    console.log("acteur", actorsCopy)
+    actorsCopy.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1
+      } else if (a.name > b.name) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+    this.setState({
+      actors: actorsCopy
+    })
+  };
+
   AddContact = () => {
     const randomIndexActors = Math.round(Math.random() * (contacts.length - 1) + 1);
-    const actorsCopy = this.state.actors;
+    const actorsCopy = [...this.state.actors];
     actorsCopy.push(contacts[randomIndexActors])
-    console.log("Hello", randomIndexActors);
+    // console.log("Hello", randomIndexActors);
     this.setState({
       actors: actorsCopy
     })
   };
 
   render() {
-    console.log(this.state.actors);
+    //console.log(this.state.actors);
     return (
       <div className="App" >
         <button onClick={this.AddContact}>Add a random contact</button>
+        <button onClick={this.SortName}>Name</button>
+        <button onClick={this.SortPopularity}>Popularity</button>
         <table>
           <thead>
             <tr>
@@ -34,10 +78,11 @@ class App extends React.Component {
           </thead>
           <tbody>
             {this.state.actors.map((actor, i) => {
-              return (<tr>
-                <td><img src={contacts[i].pictureUrl} /></td>
-                <td>{contacts[i].name}</td>
-                <td>{contacts[i].popularity}</td>
+              return (<tr key={actor.id}>
+                <td><img src={actor.pictureUrl} /></td>
+                <td>{actor.name}</td>
+                <td>{actor.popularity}</td>
+                <td><button onClick={(e) => this.delete(actor.id)}>Delete</button></td>
               </tr>)
             })}
           </tbody>
